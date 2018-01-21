7e08a0e0b797e4079c21d8da724afd02804197ed
Sharon Clapp <sbclapp@gmail.com>
2018-01-21 15:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function firstSelector(selector){
+function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 
 function nestedTarget() {
   return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+
+  let children = firstList.children
+  let start = 1
+  for (let i = 0, l = children.length; i < l; i++) {
+    expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+  }
+
+  children = secondList.children
+  start = 12
+
+  for (let i = 0, l = children.length; i < l; i++) {
+    (parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+  }
 }
